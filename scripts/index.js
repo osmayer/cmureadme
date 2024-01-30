084ec7c1ba1e23634fdddc19d06d8af1923cd955
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2024-01-30 15:46:17 -0500

    Updated for Heroku

--- scripts/index.js
+++ scripts/index.js
@@ -111,7 +111,7 @@ async function moveHeaderImage(filename, filetarget) {
 }
 
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server on...")
 })
 
